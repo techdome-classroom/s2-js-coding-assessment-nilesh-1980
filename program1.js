ed14019d264badba14d066d2a2b8af007e194254
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 14:03:55 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -31,10 +31,6 @@ function isValid(s) {
     return stack.length === 0;
 }
 
-// Example usage:
-console.log(isValid("()"));     // true
-console.log(isValid("()[]{}")); // true
-console.log(isValid("(]"));     // false
 
 module.exports = { isValid };
 
